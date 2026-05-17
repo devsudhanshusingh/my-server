7bba3e34ce579940300fbbc1d5d11fc5ae129cb1
Sudhanshu Singh <111054333+devsudhanshusingh@users.noreply.github.com>
2026-05-17 13:01:00 +0530

    Update server.js

--- src/server.js
+++ src/server.js
@@ -7,26 +7,36 @@ dotenv.config();
 
 const app = express();
 
+/* MIDDLEWARE */
+
 app.use(cors());
 
 app.use(express.json());
+
+/* ROUTE */
 
 app.get("/", (req, res) => {
   res.send("API Running");
 });
 
+/* PORT */
+
 const PORT = process.env.PORT || 5000;
+
+/* START SERVER FIRST */
+
+app.listen(PORT, () => {
+  console.log(
+    `Server running on ${PORT}`,
+  );
+});
+
+/* CONNECT DB */
 
 mongoose
   .connect(process.env.MONGO_URI)
   .then(() => {
     console.log("MongoDB Connected");
-
-    app.listen(PORT, () => {
-      console.log(
-        `Server running on ${PORT}`,
-      );
-    });
   })
   .catch((err) => {
     console.error(
