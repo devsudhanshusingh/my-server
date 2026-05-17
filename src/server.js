bc1bb5fd38957c5d770c85b0c721ff8606c132a7
Sudhanshu Singh <111054333+devsudhanshusingh@users.noreply.github.com>
2026-05-17 13:08:17 +0530

    Update comments and server start configuration

--- src/server.js
+++ src/server.js
@@ -13,7 +13,7 @@ app.use(cors());
 
 app.use(express.json());
 
-/* ROUTE */
+/* TEST ROUTE */
 
 app.get("/", (req, res) => {
   res.send("API Running");
@@ -23,15 +23,15 @@ app.get("/", (req, res) => {
 
 const PORT = process.env.PORT || 5000;
 
-/* START SERVER FIRST */
+/* START SERVER */
 
-app.listen(PORT, () => {
+app.listen(PORT, "0.0.0.0", () => {
   console.log(
     `Server running on ${PORT}`,
   );
 });
 
-/* CONNECT DB */
+/* DB CONNECTION */
 
 mongoose
   .connect(process.env.MONGO_URI)
