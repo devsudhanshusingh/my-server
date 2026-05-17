168a2c47452036f2fbeb2ccf12b3bddbd186c747
Sudhanshu Singh <111054333+devsudhanshusingh@users.noreply.github.com>
2026-05-17 13:24:27 +0530

    Refactor server.js by removing express setup
    Removed express middleware and test route from server.js.

--- src/server.js
+++ src/server.js
@@ -1,25 +1,9 @@
-import express from "express";
 import mongoose from "mongoose";
 import dotenv from "dotenv";
-import cors from "cors";
+
+import app from "./app.js";
 
 dotenv.config();
-
-const app = express();
-
-/* MIDDLEWARE */
-
-app.use(cors());
-
-app.use(express.json());
-
-/* TEST ROUTE */
-
-app.get("/", (req, res) => {
-  res.send("API Running");
-});
-
-/* PORT */
 
 const PORT = process.env.PORT || 5000;
 
